274eb1f353bba302765edff61a6fc3c7ab1b2c9e
Wendy Quarteer <wendyquarteer@outlook.com>
2022-05-19 12:28:59 +0200

    computer auto-pick (play-button removed)

--- script.js
+++ script.js
@@ -7,9 +7,6 @@ const spock = document.getElementById("spock");
 //make a string of the options for the player to use.
 const options = [rock, paper, scissors, lizard, spock];
 
-
-
-
 //add function when player clicks any of the choices.
 options.forEach(option => {
     option.addEventListener("click", function () {
@@ -20,19 +17,14 @@ options.forEach(option => {
         playerComment.innerText = (`You choose ${playerChoice}!`);
 
         //computer's choice
+        const computerComment = document.getElementById("computerComment");
         const random = Math.floor(Math.random() * 5);
         const computerChoice = options[random].title;
         console.log(computerChoice);
-       // const computerChoseTitle = computerChoice.title
-
-       // computerChose.innerText = ('Computer chose ${computerChoseTitle}');
+        computerComment.innerText = (`Computer chose ${computerChoice}!`);
     })
 });
 
-
-//prepare variable for comment each time computer makes a choice.
-//let computerChoice = document.getElementById("computerChose");
-//computerChoice.innerText = (`I choose ${ }!`)
 //prepare variable to comment the result.
 //let result = document.getElementById("winner");
 //result.innerText = (`${winner} won this game!`);
